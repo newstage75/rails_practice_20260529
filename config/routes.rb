0e16dfc26f3fc5a03b6a526524ee6dd484c11d67
nagasawa hirotomo <hirotomo.nagasawa@gmail.com>
2026-05-31 12:13:35 +0900

    リスト 3.42:HomeページをルートURLに設定する

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "static_pages#home"
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/about"
   get "static_pages/contact"
+
   #２章まで
   resources :microposts
   resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
 
-  # Defines the root path route ("/")
-  root "application#hello"
 end
